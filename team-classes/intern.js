f724299b09c007cae91d70217b97711ac0a70565
Nathan Difiori <ndifiori@gmail.com>
2022-12-07 22:15:19 -0800

    file structuring and team classes

---
+++ team-classes/intern.js
@@ -0,0 +1,20 @@
+const { getRandomValues } = require('crypto');
+const Employee = require('./employee');
+
+class Intern extends Employee {
+  constructor(name, id, email, school) {
+    super(name, id, email);
+    this.school = school;
+  }
+
+  getRole() {
+    return "Intern";
+  }
+
+  getSchool() {
+    return this.school;
+  }
+}
+
+module.exports = Intern;
+
